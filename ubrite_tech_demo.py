a729a97c3a1c4e2ab44ee6f7975a5381fd9c489c
Jelai Wang <jelaiw@uab.edu>
2019-11-26 10:49:13 -0600

    Add first pass at authors line.
    Rename section headers to improve readability.
    
    Add subheaders for PAGER parameters and results.
    
    Rename data sources parameter label to improve readability.

--- ubrite_tech_demo.py
+++ ubrite_tech_demo.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 
 st.title('U-BRITE Tech Demo')
+st.markdown('*Jelai Wang, Zongliang Yue, Abakash Samal, Dale Johnson, Patrick Dezenzio, Matt Wyatt, Christian Stackhouse, Lara Ianov, Chris Willey, Jake Chen*')
 
 # Return GBM PDX clinical data as a data frame.
 def load_clinical_data():
@@ -45,7 +46,7 @@ def run_pager(genes, sources, fdr):
 #	print(response.request.body)
 	return pd.DataFrame(response.json())
 
-st.header('Clinical data')
+st.header('Query Clinical Data')
 st.markdown("These data are read from U-BRITE's *Clinical Data Repository* programmatically from a secure call the *Unified Web Services* (UWS) API at http://ubritedvapp1.hs.uab.edu:8080/UbriteServices/getalli2b2demographics?requestorid=rdalej&cohortid=27676&format=csv.")
 
 clinical_data_load_state = st.text('Loading data ... ')
@@ -53,7 +54,7 @@ clinical_data = load_clinical_data()
 clinical_data_load_state.text('Loading data ... done!')
 st.write(clinical_data)
 
-st.header('DEG results')
+st.header('Parse DEG Results')
 st.markdown("This is from a differential gene expression analysis performed with a custom DESeq2-based pipeline (see source code for pipeline in access-controlled *Source Code Repository* at https://gitlab.rc.uab.edu/gbm-pdx/deseq2-rnaseq) on RNAseq data located in the *Omics Data Repository* (in GBM science team's dedicated project subdirectory).")
 st.markdown("These particular results are for the JX12T pairwise comparison and read from the *Documentation Repository* location at https://uab.app.box.com/folder/63730723635, a shared Box folder where the GBM analysis team shares results, slides, and other works-in-progress.")
 deg_results = load_deg_results()
@@ -63,16 +64,16 @@ if st.checkbox('Show DEG results table', value=True):
 # Remove nan from gene list.
 genes = [x for x in deg_results['symbol'].tolist() if str(x) != 'nan']
 
-st.header('PAGER results')
+st.header('Run PAGER Analysis')
 st.markdown("The list of significantly differentially expressed genes is then passed to PAGER, which offers a network-accessible REST API for performing various gene-set, network, and pathway analyses.")
 
-pager_run_state = st.text('Calling PAGER REST API ... ')
-sources = st.multiselect('Data sources',
+st.subheader('Adjust Parameters')
+sources = st.multiselect('Available Data Sources',
 	('KEGG', 'WikiPathway', 'BioCarta', 'MSigDB', 'Reactome', 'Spike'),
 	('KEGG', 'WikiPathway')
 )
-fdr = st.slider('FDR cutoff', 0.0, 1.0, 0.05, 0.01)
+fdr = st.slider('FDR Cutoff', 0.0, 1.0, 0.05, 0.01)
 pager_output = run_pager(genes, sources, fdr)
-pager_run_state.text('Calling PAGER REST API ... done!')
 
+st.subheader('View Results')
 st.write(pager_output)
